4b0a50ecdadb5920dac1790322a74d9984102cf8
Tom Salter <tomjsalter@protonmail.com>
2022-12-16 17:15:17 +0000

    Add book to myLibrary array

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let myLibrary = [];
 const newBook = document.getElementById('new-book');
 const popUp = document.querySelector('form');
 const cancelBtn = document.getElementById('cancel-btn');
+const formSubmit = document.querySelector('form')
 
 function Book(author, title, pages, read) {
     this.author = author,
@@ -31,6 +32,16 @@ newBook.addEventListener("click", () => {
 
 cancelBtn.addEventListener("click", () => {
     popUp.classList.toggle('active');
+});
+
+// [bug] on submit event, form returns element and element id, not value (text input)
+formSubmit.addEventListener("submit", function(event) {
+    event.preventDefault();
+    const author = document.getElementById("author").value;
+    const title = document.getElementById("title").value;
+    const pages = document.getElementById("pages").value;
+    const read = document.getElementById("read").value;
+    addBookToLibrary(author, title, pages, read);
 });
 
 // set of 3 placeholder books to be added into myLibrary array via addBookToLibrary function
