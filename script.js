cd90738f219c95c2f64f07c0c115e871c8f9e97f
Tom Salter <tomjsalter@protonmail.com>
2022-12-18 13:03:41 +0000

    Display book details in the console following user submission

--- script.js
+++ script.js
@@ -18,10 +18,10 @@ function addBookToLibrary() {
 
 function displayBook() {
     for (let i = 0; i < myLibrary.length; i++) {
-      const bookAuthor = myLibrary[i].author;
-      const bookTitle = myLibrary[i].title;
-      const pageCount = myLibrary[i].pages;
-      const readStatus = myLibrary[i].read;
+      const bookAuthor = myLibrary[i].author.value;
+      const bookTitle = myLibrary[i].title.value;
+      const pageCount = myLibrary[i].pages.value;
+      const readStatus = myLibrary[i].read.value;
       console.table(bookAuthor, bookTitle, pageCount, readStatus);
     }
 }
@@ -34,7 +34,6 @@ cancelBtn.addEventListener("click", () => {
     popUp.classList.toggle('active');
 });
 
-// [bug] on submit event, form returns element and element id, not value (text input)
 formSubmit.addEventListener("submit", function(event) {
     event.preventDefault();
     const author = document.getElementById("author").value;
@@ -42,13 +41,5 @@ formSubmit.addEventListener("submit", function(event) {
     const pages = document.getElementById("pages").value;
     const read = document.getElementById("read").value;
     addBookToLibrary(author, title, pages, read);
+    displayBook();
 });
-
-// set of 3 placeholder books to be added into myLibrary array via addBookToLibrary function
-const harryPotter = new Book('J. K. Rowling', 'Harry Potter', '300', 'Read');
-const lordOfTheRings = new Book('J. R. R. Tolkien', 'Lord of the Rings', '400', 'Not read');
-const dune = new Book('Frank Herbert', 'Dune', '450', 'Not read');
-
-myLibrary.push(harryPotter);
-myLibrary.push(lordOfTheRings);
-myLibrary.push(dune);
