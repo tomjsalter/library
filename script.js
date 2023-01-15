cc472536fca5b6b423e2173314ce4d59ed26c8d2
Tom Salter <tomjsalter@protonmail.com>
2023-01-15 15:25:28 +0000

    Revert to previous commit to correct reference error
    Read status and remove button now work correctly

--- script.js
+++ script.js
@@ -1,71 +1,64 @@
 let myLibrary = [];
-const newBook = document.getElementById('new-book');
-const popUp = document.querySelector('form');
-const cancelBtn = document.getElementById('cancel-btn');
-const resetBtn = document.getElementById('reset-btn');
-const form = document.querySelector('form');
-const libraryContainer = document.querySelector('main');
+const newBook = document.getElementById("new-book");
+const popUp = document.querySelector("form");
+const cancelBtn = document.getElementById("cancel-btn");
+const resetBtn = document.getElementById("reset-btn");
+const form = document.querySelector("form");
+const libraryContainer = document.querySelector("main");
 let indexValue = 0;
+cancelBtn.classList.add("change-status-btn");
+resetBtn.classList.add("change-status-btn");
 
 function Book(author, title, pages, read) {
-    this.author = author,
-    this.title = title,
-    this.pages = pages,
-    this.read = read
-    this.changeStatus = function() {
-        this.read = !this.read;
-    }
-};
- 
-function addBookToLibrary(author, title, pages, read) {
-    const book = new Book(author, title, pages, read);
-    return myLibrary.push(book);
+  (this.author = author),
+    (this.title = title),
+    (this.pages = pages),
+    (this.read = read);
+  this.changeStatus = function () {
+    this.read = !this.read;
+  };
 }
 
-function buildBookItem() {
-    for (let i = 0; i < myLibrary.length; i++) {
+function addBookToLibrary(author, title, pages, read) {
+  const book = new Book(author, title, pages, read);
+  return myLibrary.push(book);
+}
+
+function displayBook() {
+  indexValue = 0;
+  while (libraryContainer.firstChild) {
+    libraryContainer.removeChild(libraryContainer.lastChild);
+  }
+  for (let i = 0; i < myLibrary.length; i++) {
     const cardDiv = document.createElement("div");
     const bookAuthor = document.createElement("p");
     const bookTitle = document.createElement("p");
     const pageCount = document.createElement("p");
-    const removeButton = document.createElement("button");
+    const readStatus = document.createElement("p");
     const toggleStatus = document.createElement("button");
-    bookTitle.classList.add("book-title");
-    bookAuthor.classList.add("book-author");
-    pageCount.classList.add("page-count");
-    bookTitle.textContent = myLibrary[i].title;
+    const removeButton = document.createElement("button");
     bookAuthor.textContent = `Author: ${myLibrary[i].author}`;
+    bookTitle.textContent = myLibrary[i].title;
     pageCount.textContent = `Pages: ${myLibrary[i].pages}`;
-    toggleStatus.textContent = !myLibrary[i].read ? "Not read" : "Read";
-    toggleStatusClass();
+    toggleStatus.textContent = "Change status";
+    toggleStatus.classList.add("secondary-btn", "change-status");
+    removeButton.classList.add("secondary-btn", "remove");
+    bookAuthor.classList.add("book-author");
+    bookTitle.classList.add("book-title");
+    pageCount.classList.add("page-count");
+    readStatus.classList.add("read-status");
+    if (myLibrary[i].read === true) {
+      readStatus.textContent = "Status: Read";
+    } else if (myLibrary[i].read === false) {
+      readStatus.textContent = "Status: Not read";
+    }
     toggleStatus.addEventListener("click", () => {
-        myLibrary[i].changeStatus();
-        toggleStatus.textContent = !myLibrary[i].read ? "Not read" : "Read";
-        toggleStatusClass();
+      myLibrary[i].changeStatus();
+      readStatus.textContent = !myLibrary[i].read ? "Status: Not read" : "Status: Read";
     });
-    buildRemoveBtn();
+    removeButton.textContent = "Remove";
     cardDiv.setAttribute("data-index", `${indexValue}`);
     cardDiv.classList.add("book-card");
-    cardDiv.append(toggleStatus, bookTitle, bookAuthor, pageCount, removeButton);
-    libraryContainer.appendChild(cardDiv);
-    indexValue++;
-    popUp.classList.remove("active");
-    }
-}
-
-function toggleStatusClass() {
-    if (toggleStatus.textContent === "Not read") {
-      toggleStatus.classList.add("book-not-read");
-      toggleStatus.classList.remove("book-read");
-    } else if (toggleStatus.textContent === "Read") {
-      toggleStatus.classList.add("book-read");
-      toggleStatus.classList.remove("book-not-read");
-    }
-}
-
-function buildRemoveBtn() {
-    removeButton.textContent = "Remove book";
-    removeButton.classList.add("remove-btn");
     removeButton.addEventListener("click", () => {
       let cardAtt = cardDiv.getAttribute("data-index");
       cardAtt = parseInt(cardAtt);
@@ -77,41 +70,62 @@ function buildRemoveBtn() {
         }
       });
     });
-}
-
-function displayBook() {
-    indexValue = 0;
-    while (libraryContainer.firstChild) {
-      libraryContainer.removeChild(libraryContainer.lastChild);
-    }
-    buildBookItem(); 
+    cardDiv.append(
+      bookTitle,
+      bookAuthor,
+      pageCount,
+      readStatus,
+      toggleStatus,
+      removeButton
+    );
+    libraryContainer.appendChild(cardDiv);
+    indexValue++;
+    popUp.classList.remove("active");
+  }
 }
 
 newBook.addEventListener("click", () => {
-    popUp.classList.toggle('active', 'force');
+  popUp.classList.toggle("active", "force");
 });
 
 cancelBtn.addEventListener("click", () => {
-    popUp.classList.toggle('active');
+  popUp.classList.toggle("active");
 });
 
-form.addEventListener("submit", function(event) {
-    event.preventDefault();
-    const formResult = new FormData(form);
-    let author = formResult.get("author");
-    let title = formResult.get("title");
-    let pages = formResult.get("pages");
-    let read = formResult.get("read");
-    addBookToLibrary(author, title, pages, read);
-    displayBook();
-    form.reset();
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  let author = document.getElementById("author");
+  let title = document.getElementById("title");
+  let pages = document.getElementById("pages");
+  let read = document.getElementsByName("read-status");
+  author = author.value;
+  title = title.value;
+  pages = pages.value;
+  if (read[0].checked === true) {
+    read = true;
+  } else if (read[1].checked === true) {
+    read = false;
+  }
+  addBookToLibrary(author, title, pages, read);
+  displayBook();
+  form.reset();
 });
 
 // set of 4 placeholder books
-// const harryPotter = new Book('J. K. Rowling', 'Harry Potter', '300', 'Read');
-// const lordOfTheRings = new Book('J. R. R. Tolkien', 'Lord of the Rings: Battle for Middle Earth', '400', 'Not read');
-// const dune = new Book('Frank Herbert', 'Dune', '450', 'Not read');
-// const starWars = new Book('George Lucas', 'Star Wars: A New Hope', '768', 'Not read');
+// const harryPotter = new Book("J. K. Rowling", "Harry Potter", "300", "Read");
+// const lordOfTheRings = new Book(
+//   "J. R. R. Tolkien",
+//   "Lord of the Rings: Battle for Middle Earth",
+//   "400",
+//   "Not read"
+// );
+// const dune = new Book("Frank Herbert", "Dune", "450", "Not read");
+// const starWars = new Book(
+//   "George Lucas",
+//   "Star Wars: A New Hope",
+//   "768",
+//   "Not read"
+// );
 // myLibrary.push(harryPotter);
 // myLibrary.push(lordOfTheRings);
 // myLibrary.push(dune);
